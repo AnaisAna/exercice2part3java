338b226fabfcb4836a810938dfc6bd7c2f382ed5
AnaisAna <anais.fradin@laposte.net>
2019-01-25 13:42:38 +0100

    exercice2.html

---
+++ script.js
@@ -0,0 +1,10 @@
+function afficher(){
+  //Pour afficher
+  document.getElementById('text').style.display='block';
+}
+function masquer(){
+  //pour masquer
+  document.getElementById('text').style.display='none';
+}
+//pour masquer la page principal
+  document.getElementById('text').style.display='none';
